2767648ce1e54ee4170f0dbdfd82758ce467a941
c-x-he <70553289+c-x-he@users.noreply.github.com>
2023-05-17 10:34:36 -0700

    Update hdWGCNA_Ast_marker_overlap.R

--- hdWGCNA_Ast_marker_overlap.R
+++ hdWGCNA_Ast_marker_overlap.R
@@ -1,5 +1,8 @@
-#https://github.com/smorabit/hdWGCNA/blob/dev/vignettes/basic_tutorial.Rmd
+# Marker Overlap Analysis
+# Code adapted by Caroline He from https://github.com/smorabit/hdWGCNA/blob/dev/vignettes/basic_tutorial.Rmd
+#2023
 
+## 1. load packages
 library(Seurat)
 library(tidyverse)
 library(cowplot)
@@ -9,33 +12,33 @@ library(hdWGCNA)
 library(enrichR)
 library(GeneOverlap)
 
-# set random seed for reproducibility
+## 2. Set random seed for reproducibility
 set.seed(12345)
 
+## 3. Set working directory
 setwd("~/new_frontal_official/hdWGCNA/Ast-subset")
 
+## 4. Load the seurat object that is made at the end of the hdWGCNA-module_identification.R script
 seurat_obj <- readRDS("~/new_frontal_official/hdWGCNA/Ast-subset/hdWGCNA-seurat_obj-basic-end.rds")
 
-# compute cell-type marker genes with Seurat:
+## 5. Load the list of marker genes associated with each astrocyte cluster
 Ast_clusters_pos <- read_csv("Ast_clusters_pos.csv")
 Ast_clusters_neg <- read_csv("Ast_clusters_neg.csv")
 
 markers <- as.data.frame(Ast_clusters_pos)
   
-# compute marker gene overlaps
+## 6. compute marker gene overlaps
 overlap_df <- OverlapModulesDEGs(
   seurat_obj,
   deg_df = markers,
   fc_cutoff = 0.25 # log fold change cutoff for overlap analysis
 )
 
-#setwd("~/new_frontal_official/hdWGCNA/Ast-subset/")
-#write.csv(overlap_df, "Ast-cluster_overlap_DOWN.csv")
+write.csv(overlap_df, "Ast-cluster_overlap_UP.csv")
 
-# overlap barplot, produces a plot for each cell type
+## 7. Visualize the overlap and associated statistics with the gene list comparison
 plot_list <- OverlapBarPlot(overlap_df)
 
-# stitch plots with patchwork
 wrap_plots(plot_list, ncol=3)
 
 OverlapDotPlot(
